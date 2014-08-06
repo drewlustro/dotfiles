02de5a38b7d9ff67955ba9a4be680e76084e9294
Drew Lustro <drewlustro@gmail.com>
2014-08-06 03:38:58 -0400

    Add ll alias back in

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -53,6 +53,7 @@ fi
 
 # List all files colorized in long format
 alias l="ls -lF ${colorflag}"
+alias ll="ls -lF ${colorflag}"
 
 # List all files colorized in long format, including dot files
 alias la="ls -laF ${colorflag}"

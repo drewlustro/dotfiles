8ddb66a2b8cf1ec9a0a65974bd0b911def1f8f53
Drew Lustro <drewlustro@gmail.com>
2016-05-02 17:11:27 -0400

    More linux fresh-install packages. New 'ainstall' alias

--- linux/fresh-install.sh
+++ linux/fresh-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#!/bin/bash
+FRESH_UBUNTU() {
+
+sudo apt-get install -y synaptic compizconfig-settings-manager compiz-plugins-extra unity-tweak-tool
+
+}
 
 # linux headers
 sudo apt-get install -y linux-headers-$(uname -r)
@@ -12,18 +16,79 @@ sudo apt-get install -y build-essential
 sudo apt-get install -y ubuntu-restricted-extras openjdk-8-jdk flac lame automake libtool
 
 # apps
-sudo apt-get install -y git synaptic gdebi vim  pidgin skype transmission compizconfig-settings-manager compiz-plugins-extra unity-tweak-tool clementine
+sudo apt-get install -y \
+audacity \
+clementine \
+gdebi \
+git \
+pidgin \
+transmission \
+vim \
+vlc \
+;
+
 
 # fonts
 sudo apt-get install -y fonts-inconsolata ttf-anonymous-pro fonts-roboto fonts-droid
 # kill cache
 fc-cache -f -v
 
+# python
+sudo apt-get install python-pip python-dev
+sudo pip install --upgrade pip
+sudo pip install virtualenv
+sudo pip install pylint
 
-# get node
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+# node 4.x LTS
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt-get install -y npm
 
 # npm packges
 npm install -g bower yo grunt-cli gulp babel webpack npm-check-updates
 
+# essentials
+sudo apt-get install -y \
+autoconf \
+automake \
+cmake \
+curl \
+ffmpeg \
+flac \
+g++ \
+libasound2 \
+libasound2-plugins \
+libboost-dev \
+libboost-python-dev \
+libcairo2 \
+libcairo2-dev \
+libjpeg-dev \
+libjpeg8 \
+openssh-client \
+openssh-server \
+openssh-sftp-server \
+openssl \
+ppa-purge \
+pylint \
+samba \
+samba-dsdb-modules \
+samba-libs \
+samba-vfs-modules \
+shotwell \
+smbclient \
+smbnetfs \
+ttf-mscorefonts-installer \
+wget \
+wine \
+wmctrl \
+xclip \
+xdotool \
+zlib1g \
+zlib1g-dev \
+;
+
+# y-ppa-manager
+
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager
+sudo apt-get update
+sudo apt-get install -y y-ppa-manager

5ebfe54b78a005a2ad055c249af66c10407bf664
Drew Lustro <drew@angel.co>
2018-12-11 18:09:05 -0800

    More config files (fzf, mackup, aliases to exa)

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -33,7 +33,7 @@ alias treee="tree -hL 2 --filelimit 20 --dirsfirst"
 # ls Helpers
 
 # Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU coreutils `ls`
+if [ -x "$(which exa)" ] || ls --color > /dev/null 2>&1; then # rust `exa` or GNU coreutils `ls`
     colorflag="--color=auto -hF";
     if [ -f "$HOME/.shell/LS_COLORS" ]; then
         eval $( dircolors -b $HOME/.shell/LS_COLORS );
@@ -41,12 +41,6 @@ if ls --color > /dev/null 2>&1; then # GNU coreutils `ls`
 elif [ "$PLATFORM" = "osx" ]; then # OS X `ls`
     colorflag="-G"
     export LS_COLORS="no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:"
-
-    # Flush Directory Service cache
-    alias flushcache="dscacheutil -flushcache && killall -HUP mDNSResponder"
-
-    # Clean up LaunchServices to remove duplicates in the “Open With” menu
-    alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
 fi;
 
 # exa = `ls` replacement written in rust
@@ -203,13 +197,16 @@ if [ "$PLATFORM" = "osx" ]; then
     # Lock the screen (when going AFK)
     alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 
-    # Make Grunt print stack traces by default
-    command -v grunt > /dev/null && alias grunt="grunt --stack"
-
     # Allow instantiating duplicate Slack.app instances (for multiple teams)
     if [ -s "/Applications/Slack.app" ]; then
         alias slack-dupe="open -n /Applications/Slack.app"
     fi;
+
+    # Flush Directory Service cache
+    alias flushcache="dscacheutil -flushcache && killall -HUP mDNSResponder"
+
+    # Clean up LaunchServices to remove duplicates in the “Open With” menu
+    alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
 
     alias canary-no-security="open -a Google\ Chrome\ Canary --args --disable-web-security --user-data-dir";
 

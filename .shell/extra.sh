8560749629878c543909b460f5a210c2d583f505
Drew Lustro <drew@angel.co>
2019-03-07 23:22:40 -0800

    Don't automatically load virtualenvwrapper

--- .shell/extra.sh
+++ .shell/extra.sh
@@ -1,9 +1,9 @@
 # extra.sh
 
 # virtualenvwrapper support
-VEWPATH=$(which virtualenvwrapper.sh);
-[ -f "$VEWPATH" ] && [ -x "$VEWPATH" ] && source "$VEWPATH";
-unset VEWPATH;
+# VEWPATH=$(which virtualenvwrapper.sh);
+# [ -f "$VEWPATH" ] && [ -x "$VEWPATH" ] && source "$VEWPATH";
+# unset VEWPATH;
 
 # Rust environment support
 [ -d "$HOME/.cargo" ] && source $HOME/.cargo/env;

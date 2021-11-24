debe5e410944ec775243714252e3f46f44c3b7a0
Drew Lustro <drew@angel.co>
2021-11-23 19:43:49 -1000

    fix indentation

--- .shell/path.sh
+++ .shell/path.sh
@@ -1,22 +1,22 @@
 # path.sh
 
 if [ "$PLATFORM" = "osx" ]; then
-    # Prepend GNU Coreutils path and manpath to existing $PATH/$MANPATH
-    if [ -x "$(which brew)" ] && [ -d "$(brew --prefix coreutils)/libexec/gnubin" ]; then
-        # Add GNU coreutils path/man (installed with 'brew install coreutils')
-        COREUTILS_BIN="$(brew --prefix coreutils)/libexec/gnubin";
-        COREUTILS_MAN="$(brew --prefix coreutils)/libexec/gnuman";
-        [ -d $COREUTILS_BIN ] && export PATH="$COREUTILS_BIN:$PATH";
-        [ -d $COREUTILS_MAN ] && export MANPATH="$COREUTILS_MAN:$MANPATH";
-    fi;
+  # Prepend GNU Coreutils path and manpath to existing $PATH/$MANPATH
+  if [ -x "$(which brew)" ] && [ -d "$(brew --prefix coreutils)/libexec/gnubin" ]; then
+    # Add GNU coreutils path/man (installed with 'brew install coreutils')
+    COREUTILS_BIN="$(brew --prefix coreutils)/libexec/gnubin";
+    COREUTILS_MAN="$(brew --prefix coreutils)/libexec/gnuman";
+    [ -d $COREUTILS_BIN ] && export PATH="$COREUTILS_BIN:$PATH";
+    [ -d $COREUTILS_MAN ] && export MANPATH="$COREUTILS_MAN:$MANPATH";
+  fi;
 
-    # If Calibre is installed, provide CLI tools for converting books
-    if [ -d "/Applications/calibre.app/Contents/console.app/Contents/MacOS" ]; then
-      export PATH="$PATH:/Applications/calibre.app/Contents/console.app/Contents/MacOS";
-    fi;
+  # If Calibre is installed, provide CLI tools for converting books
+  if [ -d "/Applications/calibre.app/Contents/console.app/Contents/MacOS" ]; then
+    export PATH="$PATH:/Applications/calibre.app/Contents/console.app/Contents/MacOS";
+  fi;
 
-    # support homebrew-installed python3
-    [ -d "/usr/local/opt/python/libexec/bin" ] && export PATH="/usr/local/opt/python/libexec/bin:$PATH";
+  # support homebrew-installed python3
+  [ -d "/usr/local/opt/python/libexec/bin" ] && export PATH="/usr/local/opt/python/libexec/bin:$PATH";
 fi;
 
 # setup PATH to check $HOME/bin first, (then /usr/local/bin, /usr/bin ... etc)
